49038dde744022e60ca916ade92cb1a08895be24
246_135 <My_email_adress@gmail.com>
2017-06-05 17:29:11 -0700

    Changed AnagramCPD3

---
+++ src/main/java/core/A.java
@@ -0,0 +1,20 @@
+package core;
+
+public class A {
+	public void foo()  {
+		
+		for (int i = 0; i < 10; i++) {
+				
+			for (int k = 0; k < 20; i++) {
+					
+					System.out.println("Hello");
+					
+				}
+			}
+		}
+		
+		void bar() {
+			for (;true;);
+		}
+
+}
